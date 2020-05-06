a0bee7b4d6b5a04e464d5d7489fbb5280b8937a8
Devin Strickland <dvnstrcklnd@users.noreply.github.com>
2020-05-05 21:15:51 -0700

    Aquadoc initial commit

---
+++ libraries/Thermocyclers.rb
@@ -0,0 +1,140 @@
+# Library for handling thermocyclers, including qPCR thermocyclers
+#
+# @author Devin Strickland <strcklnd@uw.edu>
+
+needs "Thermocyclers/TestThermocycler"
+needs "Thermocyclers/BioRadCFX96"
+
+# Helper module for standard thermocycler procedures
+#
+# @author Devin Strickland <strcklnd@uw.edu>
+# @author Eriberto Lopez <elopez3@uw.edu>
+# @note methods originally deployed as `QPCR_ThermocyclerLib`
+#   on UW BIOFAB production 10/05/18
+module ThermocyclerHelper
+
+  # Steps for setting up a proram in a thermocycler
+  #
+  # @param thermocycler [Thermocycler]
+  # @param program [PCRProgram]
+  # @param composition [PCRComposition]
+  def set_up_program(thermocycler:, program:, composition:)
+    show do
+      title "Set Up #{thermocycler.model} Thermocycler"
+
+      note thermocycler.open_software
+      image thermocycler.open_software_image
+      separator
+
+      # TODO: What happens if this is not a qPCR experiment?
+      note thermocycler.set_dye(composition: composition)
+      image thermocycler.setup_workspace_image
+      separator
+
+      note thermocycler.select_program_template(program: program)
+      image thermocycler.setup_program_image
+      separator
+
+      # TODO: What happens if this is not a qPCR experiment?
+      note thermocycler.select_layout_template(program: program)
+      image thermocycler.setup_plate_layout_image
+    end
+  end
+
+  # Steps for loading physical tubes or plates into a thermocycler
+  #
+  # @param thermocycler [Thermocycler]
+  # @param items [Item, Array<Item>]
+  # @param filename [String] the filename to safe the experiment file 
+  def load_plate_and_start_run(thermocycler:, items: [], filename: nil)
+
+    # Normalize the presentation of `items`
+    items = [items] if items.respond_to?(:collection?)
+
+    show do
+      title "Start Run on #{thermocycler.model} Thermocycler"
+      
+      note thermocycler.open_lid
+      image thermocycler.open_lid_image
+      separator
+
+      # TODO: Make this work for plates, stripwells, and individual tubes
+      if items.length == 1 && is_96well_plate?(items.first)
+        note thermocycler.place_plate_in_instrument(plate: items.first)
+        warning thermocycler.confirm_plate_orientation
+      else
+        note 'Place samples in thermocycler'
+      end
+      separator
+
+      note thermocycler.close_lid
+      image thermocycler.close_lid_image
+      separator
+
+      note thermocycler.start_run
+      # TODO: What happens if this is not a qPCR experiment?
+      note thermocycler.save_experiment_file(filename: filename) if filename.present?
+      image thermocycler.start_run_image
+    end
+  end
+
+  # Export the measurements, if a qPCR run
+  #
+  # @param thermocycler [Thermocycler]
+  def export_measurements(thermocycler:)
+    show do
+      title "Export Measurements"
+      
+      note "Once the run has finished, export the measurements"
+      note thermocycler.export_measurements()
+      image thermocycler.export_results_image
+    end
+  end
+
+  # TODO: A method from Eriberto Lopez's code that I haven't implemented yet
+  # def upload_measurments(experiment_name)
+  #   upload_filename = experiment_name + " - Quantification Summary_0.csv" # Suffix of file will always be the same
+  #   up_show, up_sym = upload_show(upload_path = EXPORT_FILEPATH, upload_filename)
+  #   if debug
+  #     upload = Upload.find(11278) # Dummy data set
+  #   else
+  #     upload = find_upload_from_show(up_show, up_sym)
+  #   end
+  #   return upload
+  # end
+
+  private
+
+  # Test whether an item is a 96 well plate
+  #
+  # @param item [Item]
+  # @return [Boolean]
+  def is_96well_plate?(item)
+    item.collection? && item.capacity == 96
+  end
+end
+
+# Factory class for building thermocycler objects
+#
+# @author Devin Strickland <strcklnd@uw.edu>
+class ThermocyclerFactory
+
+  # Instantiates a new `Thermocycler`
+  #
+  # @param model [String] the `MODEL` of the thermocycler. Must match the 
+  #   constant `MODEL` in an exisiting thermocycler class.
+  # @return [Thermocycler]
+  def self.build(model:)
+    case model
+    when TestThermocycler::MODEL
+      TestThermocycler.new()
+    when BioRadCFX96::MODEL
+      BioRadCFX96.new()
+    else
+      msg = "Unrecognized Thermocycler Model: #{model}"
+      raise ThermocyclerInputError.new(msg)
+    end
+  end
+end
+
+class ThermocyclerInputError < ProtocolError; end
