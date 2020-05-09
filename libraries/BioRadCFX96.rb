8eb20b2d9b169244855f96562709549067ebd9b5
Devin Strickland <dvnstrcklnd@users.noreply.github.com>
2020-05-09 14:45:17 -0700

    Aquadoc update

--- libraries/BioRadCFX96.rb
+++ libraries/BioRadCFX96.rb
@@ -6,9 +6,12 @@ needs "Thermocyclers/AbstractThermocycler"
 
 class BioRadCFX96 < AbstractThermocycler
 
+  include QPCRMixIn
+
   MODEL = "BioRad CFX96"
   PROGRAM_EXT = ".prcl"
   LAYOUT_EXT =  ".pltd"
+  SOFTWARE_NAME = "CFX Manager Software"
 
   # Instantiates the class
   #
@@ -34,5 +37,107 @@ class BioRadCFX96 < AbstractThermocycler
       start_run_image: "start_run.png",
       export_results_image: "exporting_qPCR_quantification.png"
     }
+  end
+
+  ########## Language Methods
+  # These methods are not very specific and will probably need to be overridden
+  #   in the concrete classes.
+
+  # Instructions for turning on the thermocycler
+  #
+  # @return [String]
+  def turn_on()
+    "If the thermocycler is off, toggle the power switch in the back of the" \
+    " instrument"
+  end
+
+  # Instructions for opening the software that controls the thermocycler
+  #
+  # @return [String]
+  def open_software()
+    "Open #{software_name}"
+  end
+
+  # Instructions for setting the dye channel on a qPCR thermocycler
+  #
+  # @param composition [PCRComposition]
+  # @param dye_name [String] can be supplied instead of a `PCRComposition`
+  # @return [String]
+  # @todo should be moved to MixIn
+  def set_dye(composition: nil, dye_name: nil)
+    dye_name = composition.dye.try(:input_name) || dye_name
+    "Click on the <b>PrimePCR</b> and choose <b>#{dye_name}</b> as the dye"
+  end
+
+  # Instructions for selecting the PCR program template in the software
+  #
+  # @param program [PCRProgram]
+  # @return [String]
+  def select_program_template(program:)
+    file = program_template_file(program: program)
+    "Choose the program template <b>#{file}</b>"
+  end
+
+  # Instructions for selecting the plate layout template in the software
+  #
+  # @param program [PCRProgram]
+  # @return [String]
+  def select_layout_template(program:)
+    file = layout_template_file(program: program)
+    "Choose the layout template <b>#{file}</b>"
+  end
+
+  # Instructions for placing a plate in the instrument
+  #
+  # @param plate [Collection]
+  # @return [String]
+  def place_plate_in_instrument(plate:)
+    "Place plate #{plate} in the thermocycler"
+  end
+
+  # Instructions for confirming the orientation of a plate in the instrument
+  #
+  # @return [String]
+  def confirm_plate_orientation()
+    "MAKE SURE THAT THE PLATE IS IN THE CORRECT ORIENTATION"
+  end
+
+  # Instructions for opening the lid
+  #
+  # @return [String]
+  def open_lid()
+    "Click the <b>Open Lid</b> button"
+  end
+
+  # Instructions for closing the lid
+  #
+  # @return [String]
+  def close_lid()
+    "Click the <b>Close Lid</b> button"
+  end
+
+  # Instructions for starting the run
+  #
+  # @return [String]
+  def start_run()
+    "Click the <b>Start Run</b> button"
+  end
+
+  # Instructions for saving an experiment file
+  #
+  # @param filename [String] the name of the file (without the full path)
+  # @return [String]
+  def save_experiment_file(filename:)
+    "Save the experiment as #{filename} in #{params[:experiment_filepath]}"
+  end
+
+  # Instructions for exporting measurements from a qPCR run
+  #
+  # @return [String]
+  def export_measurements()
+    "Click <b>Export</b><br>" +
+    "Select <b>Export All Data Sheets</b><br>" +
+    "Export all sheets as CSV<br>" +
+    "Save files to the #{params[:export_filepath]} directory"
   end
 end
