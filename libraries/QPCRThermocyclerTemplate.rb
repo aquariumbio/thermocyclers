7d0eec486466b5a670eeb9a9c0884347ca82f870
Devin Strickland <dvnstrcklnd@users.noreply.github.com>
2020-05-15 10:56:35 -0700

    Aquadoc update

--- libraries/QPCRThermocyclerTemplate.rb
+++ libraries/QPCRThermocyclerTemplate.rb
@@ -1,38 +1,38 @@
-# Template for making new qPCR thermocycler classes
-#
-# @author Devin Strickland <strcklnd@uw.edu>
+# frozen_string_literal: true
 
 needs 'Thermocyclers/AbstractThermocycler'
 
+# Template for making new qPCR thermocycler classes
+#
+# @author Devin Strickland <strcklnd@uw.edu>
 class ThermocyclerTemplate < AbstractThermocycler
-
   # CONSTANTS that really shouldn't ever change
-  MODEL = ""
-  PROGRAM_EXT = ""
-  LAYOUT_EXT =  ""
-  SOFTWARE_NAME = ""
+  MODEL = ''
+  PROGRAM_EXT = ''
+  LAYOUT_EXT =  ''
+  SOFTWARE_NAME = ''
 
   # Instantiates the class
   #
   # @return [ThermocyclerTemplate]
-  def initialize()
+  def initialize
     super()
   end
 
   # Lab-specific, user-defined parameters
   #
   # @return [Hash]
-  def user_defined_params()
+  def user_defined_params
     {
-      experiment_filepath: "Desktop/my_experiment_path",
-      export_filepath: "Desktop/my_export_path",
-      image_path: "Actions/ThermocyclerTemplate"
+      experiment_filepath: 'Desktop/my_experiment_path',
+      export_filepath: 'Desktop/my_export_path',
+      image_path: 'Actions/ThermocyclerTemplate'
     }
   end
 
   ########## Language Methods
   # These methods are not very specific and will probably need to be overridden
-  #   in the concrete classes. 
+  #   in the concrete classes.
 
   # # Instructions for turning on the thermocycler
   # #
@@ -130,5 +130,4 @@ class ThermocyclerTemplate < AbstractThermocycler
   #   "Export all sheets as CSV<br>" +
   #   "Save files to the #{params[:export_filepath]} directory"
   # end
-
 end
