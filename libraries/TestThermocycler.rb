b4bc8c769eb001217b9228c8a8cb8f84dafd8aaf
Devin Strickland <dvnstrcklnd@users.noreply.github.com>
2020-05-15 10:56:36 -0700

    Aquadoc update

--- libraries/TestThermocycler.rb
+++ libraries/TestThermocycler.rb
@@ -1,35 +1,38 @@
+# frozen_string_literal: true
+
 needs 'Thermocyclers/AbstractThermocycler'
 
+# Class to use as a generic qPCR thermocycler for testing
+#
+# @author Devin Strickland <strcklnd@uw.edu>
 class TestThermocycler < AbstractThermocycler
-
   include QPCRMixIn
 
   # CONSTANTS that really shouldn't ever change
-  MODEL = "Test Model"
-  PROGRAM_EXT = ".abc"
-  LAYOUT_EXT =  "xyz" # Intentionally missing leading dot for test
-  SOFTWARE_NAME = "Test Software"
+  MODEL = 'Test Model'
+  PROGRAM_EXT = '.abc'
+  LAYOUT_EXT =  'xyz' # Intentionally missing leading dot for test
+  SOFTWARE_NAME = 'Test Software'
 
   # Instantiates the class
   #
   # @return [TestThermocycler]
-  def initialize()
+  def initialize
     super()
   end
 
   # Lab-specific, user-defined parameters
   #
   # @return [Hash]
-  def user_defined_params()
+  def user_defined_params
     {
-      experiment_filepath: "Desktop/test_experiment_path",
-      export_filepath: "Desktop/test_export_path",
-      image_path: "Actions/TestThermocycler"
+      experiment_filepath: 'Desktop/test_experiment_path',
+      export_filepath: 'Desktop/test_export_path',
+      image_path: 'Actions/TestThermocycler'
     }
   end
 
-  def say_hello()
+  def say_hello
     "Hello, my name is #{MODEL}"
   end
-
 end
