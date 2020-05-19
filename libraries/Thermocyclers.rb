19ad50b64704a28c68c1227f593101c9ff61a2b5
Devin Strickland <dvnstrcklnd@users.noreply.github.com>
2020-05-19 06:00:17 -0700

    Aquadoc update

--- libraries/Thermocyclers.rb
+++ libraries/Thermocyclers.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 # Library for handling thermocyclers, including qPCR thermocyclers
-#
 # @author Devin Strickland <strcklnd@uw.edu>
 
 needs 'Thermocyclers/TestThermocycler'
@@ -20,22 +19,21 @@ module ThermocyclerHelper
   # @param thermocycler [Thermocycler]
   # @param program [PCRProgram]
   # @param composition [PCRComposition]
-  def set_up_program(thermocycler:, program:, composition:)
+  # @param qpcr [Boolean] whether to call setup methods specific to a
+  #   qPCR experiment: `open_software`, `set_dye`, `select_layout_template`,...
+  # @return [void]
+  def set_up_program(thermocycler:, program:, composition:, qpcr: false)
     show do
       title "Set Up #{thermocycler.model} Thermocycler"
 
-      if thermocycler.respond_to?(:open_software)
+      if qpcr
         note thermocycler.open_software
         image thermocycler.open_software_image
         separator
-      end
 
-      if thermocycler.respond_to?(:set_dye)
         note thermocycler.set_dye(composition: composition)
         separator
-      end
 
-      if thermocycler.respond_to?(:setup_workspace_image)
         image thermocycler.setup_workspace_image
         separator
       end
@@ -44,7 +42,7 @@ module ThermocyclerHelper
       image thermocycler.setup_program_image
       separator
 
-      if thermocycler.respond_to?(:set_dye)
+      if qpcr
         note thermocycler.select_layout_template(program: program)
         image thermocycler.setup_plate_layout_image
       end
@@ -56,7 +54,9 @@ module ThermocyclerHelper
   # @param thermocycler [Thermocycler]
   # @param items [Item, Array<Item>]
   # @param filename [String] the filename to safe the experiment file
-  def load_plate_and_start_run(thermocycler:, items: [], filename: nil)
+  # @return [void]
+  def load_plate_and_start_run(thermocycler:, items: [],
+                               experiment_filename: nil)
     # Normalize the presentation of `items`
     items = [items] if items.respond_to?(:collection?)
     plate = single_96well_plate?(items)
@@ -82,8 +82,8 @@ module ThermocyclerHelper
       separator
 
       note thermocycler.start_run
-      if filename.present? && thermocycler.respond_to?(:save_experiment_file)
-        note thermocycler.save_experiment_file(filename: filename)
+      if experiment_filename.present?
+        note thermocycler.save_experiment_file(filename: experiment_filename)
       end
     end
   end
@@ -91,15 +91,14 @@ module ThermocyclerHelper
   # Export the measurements, if a qPCR thermocycler
   #
   # @param thermocycler [Thermocycler]
+  # @return [void]
   def export_measurements(thermocycler:)
-    return unless thermocycler.respond_to?(:export_measurements)
-
     show do
       title 'Export Measurements'
 
       note 'Once the run has finished, export the measurements'
       note thermocycler.export_measurements
-      image thermocycler.export_results_image
+      image thermocycler.export_measurements_image
     end
   end
 
